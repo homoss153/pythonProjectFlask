9d0b4a9438c61411c9a27a6aca881abc3c997e93
co4an <homoss855@example.com>
2024-02-22 13:48:40 +0300

    1

--- main.py
+++ main.py
@@ -225,4 +225,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", ssl_context=('cert.pem', 'key.pem'), debug=True, port=443)
+    app.run(host="0.0.0.0", debug=False, port=80)
